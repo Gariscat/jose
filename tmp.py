6addacb9563171fd8f1f614bb7252c8e8546d5da
Gariscat <xl3133@nyu.edu>
2023-07-11 05:34:00 +0000

    test-humanoid-control

--- tmp.py
+++ tmp.py
@@ -3,20 +3,37 @@ import time
 
 import mujoco
 import mujoco.viewer
+import numpy as np
+import gym
+
+env = gym.make('Humanoid-v2')
 
 xml_path = 'humanoid.xml'
+frame_skip = 5
 m = mujoco.MjModel.from_xml_path(xml_path)
 d = mujoco.MjData(m)
+
+
+### print(dir(m))
 
 with mujoco.viewer.launch_passive(m, d) as viewer:
   # Close the viewer automatically after 30 wall-seconds.
   start = time.time()
   while viewer.is_running() and time.time() - start < 30:
     step_start = time.time()
+    action = env.action_space.sample()
+    print()
+    d.ctrl[:] = action
+    mujoco.mj_step(m, d, nstep=frame_skip)
 
+    # As of MuJoCo 2.0, force-related quantities like cacc are not computed
+    # unless there's a force sensor in the model.
+    # See https://github.com/openai/gym/issues/1541
+    mujoco.mj_rnePostConstraint(m, d)
     # mj_step can be replaced with code that also evaluates
     # a policy and applies a control signal before stepping the physics.
-    mujoco.mj_step(m, d)
+    #### mujoco.mj_step(m, d)
+    print(d.geom('head'))
 
     # Example modification of a viewer option: toggle contact points every two seconds.
     with viewer.lock():
